a045d179b5e29bccafc482da1e4831a5e4b6f1f1
Bob Miller <kbob@jogger-egg.com>
2013-10-05 11:50:46 -0700

    Trivial cosmetic change.

--- motor-util.h
+++ motor-util.h
@@ -7,7 +7,7 @@
 #define uSEC_per_uSTEP(mm_per_sec) \
                            ((uint16_t)(25.4 / 2000 / (mm_per_sec) * 1e6))
 #define TICKS_per_uSTEP(mm_per_sec) \
-    (F_CPU * 25.4 / 2000.0 / (mm_per_sec))
+                           (F_CPU * 25.4 / 2000.0 / (mm_per_sec))
 
 
 extern void move_x  (int32_t usteps);
