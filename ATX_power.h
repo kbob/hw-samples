9b49f93695eec352e91eaa15bf8c2505e0f2d76f
Bob Miller <kbob@jogger-egg.com>
2014-03-20 07:04:11 -0700

    Now uses ATX Power Ready signal.

--- ATX_power.h
+++ ATX_power.h
@@ -10,11 +10,14 @@ static inline void init_ATX_power(void)
     PORTB |= _BV(PORTB6);
     DDRB  |= _BV(DDB6);
     PORTB |= _BV(PORTB6);
+
+    DDRA &= ~_BV(DDA0);         // pin is input
+    PORTA &= ~_BV(PORTA0);      // no pullup
 }
 
 static inline bool ATX_power_state(void)
 {
-    return bit_is_clear(PORTB, PORTB6);
+    return bit_is_set(PINA, PINA0);
 }
 
 static inline void enable_ATX_power(void)
